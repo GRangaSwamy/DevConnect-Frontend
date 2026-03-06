dae0d07f3638edc081b8e190388aa7e71ec9cfd6
Ranga Swamy <rangasamygolla06@gmail.com>
2026-03-06 18:51:44 +0530

    first commit

--- learngs.md
+++ learngs.md
@@ -4,3 +4,31 @@ created a Hello World in place of all
 
 configured Tailwind
 intalled and explored Daisy UI and tested with navbar
+
+created(copy & paste) a navbar using DaisyUI.
+Installed React Router Dom
+Tested some example routes
+
+
+Created a brwoser router ==>
+    BrowserRouter ==> Routes ==> Route(path,ele) => Outlet 
+
+
+
+ -Login Page :- 
+    Craeted a component using DaisyUI
+    Installed axios
+    installed CORS and set at backend 
+    Successfully make a call to login api
+    Verified cookies are set succesfully under Application tab in dev console
+
+
+
+
+
+Body :-
+    NavBar
+    Route = / ➡️ Feed
+    Route = /login ➡️ Login
+    Route = /connections ➡️ Connections
+    Route = /profile ➡️ Profile
---
+++ src/components/login .jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+const Login = () => {
+    const[email,setEmail] = useState("puli@gmail.com");
+    const[password,setPassword] = useState("Pulii@24");
+    const handleLogin = async ()=>{
+         try{
+           const res = axios.post('http://localhost:3000/login',{
+            email,password
+          },{withCredentials:true})
+         }catch(err){
+            console.log("Error in Login",err)
+         }
+    }
+  return (
+    <div className="flex justify-center mt-4">
+        <div className="card card-border bg-base-300 w-96">
+            <div className="card-body">
+                <h2 className="card-title justify-center">Login</h2>
+                <fieldset className="fieldset">
+                    <legend className="fieldset-legend">Email ID : </legend>
+                    <input type="text" value={email} className="input" placeholder="Type email here" onChange={(e)=>setEmail(e.target.value)} />
+                </fieldset>
+                <fieldset className="fieldset">
+                    <legend className="fieldset-legend">password</legend>
+                    <input type="text" value={password} className="input" placeholder="Type email here" onChange={(e)=>{
+                        setPassword(e.target.value)
+                    }} />
+                </fieldset>
+                <div className="card-actions flex justify-center ">
+                   <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                </div>
+            </div>
+    </div>
+    </div>
+  );
+}
+export default Login;
